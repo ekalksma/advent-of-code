6dbdbabaf5a50802d3d8348afd050fc20be1a509
ekalksma <emiel.kalksma@gmail.com>
2021-12-02 00:27:40 +0100

    add day 1 solution

--- 2021/day01/solution.py
+++ 2021/day01/solution.py
@@ -1,2 +1,24 @@
 def part1(data):
-    return 0
+    input = data.splitlines()
+    increased = 0
+
+    for i in range(len(input)):
+        if i+1 > (len(input) -1):
+            break
+        if int(input[i]) < int(input[i+1]):
+            increased += 1
+    
+    return increased
+
+
+def part2(data):
+    input = data.splitlines()
+    increased = 0
+
+    for i in range(len(input)):
+        if i+3 > (len(input) -1):
+            break
+        if int(input[i]) < int(input[i+3]):
+            increased += 1
+    
+    return increased
