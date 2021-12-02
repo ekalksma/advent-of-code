a27141a6ee0380b85fe828f175e91de60fdcf4ec
ekalksma <emiel.kalksma@gmail.com>
2021-12-02 10:43:00 +0100

    Add day02 solution part2

--- 2021/day02/solution.py
+++ 2021/day02/solution.py
@@ -14,3 +14,23 @@ def part1(data):
             depth -= units
 
     return hpos * depth
+
+
+def part2(data):
+    input = data.splitlines()
+    depth = 0
+    hpos = 0
+    aim = 0
+    
+    for direction in input:
+        units = int(direction[(len(direction)-1)])
+
+        if "forward" in direction:
+            hpos += units
+            depth += (aim * units)
+        elif "down" in direction:
+            aim += units
+        elif "up" in direction:
+            aim -= units
+
+    return hpos * depth
