e5784fdcaf79b6030e0c9dbec0bd026400b1af57
ekalksma <emiel.kalksma@gmail.com>
2021-12-02 01:25:26 +0100

    Refactor day01

--- 2021/day01/solution.py
+++ 2021/day01/solution.py
@@ -1,24 +1,22 @@
 def part1(data):
-    input = data.splitlines()
-    increased = 0
-
-    for i in range(len(input)):
-        if i+1 > (len(input) -1):
-            break
-        if int(input[i]) < int(input[i+1]):
-            increased += 1
-    
-    return increased
+    input = list(map(int, data.splitlines()))
+    return num_depth_increased(input, 1)
 
 
 def part2(data):
-    input = data.splitlines()
-    increased = 0
+    input = list(map(int, data.splitlines()))
+    return num_depth_increased(input, 3)
 
-    for i in range(len(input)):
-        if i+3 > (len(input) -1):
+
+def num_depth_increased(input, window_size):
+    increased = 0
+    window_size = window_size
+    input_length = len(input)
+
+    for i in range(input_length):
+        if i + window_size > (input_length -1):
             break
-        if int(input[i]) < int(input[i+3]):
+        if input[i] < input[i + window_size]:
             increased += 1
     
     return increased
