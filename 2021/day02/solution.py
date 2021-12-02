acfa91f12666e3101096c2200abe8f7609502539
ekalksma <emiel.kalksma@gmail.com>
2021-12-02 10:30:51 +0100

    Add day02 part 1 solution

--- 2021/day02/solution.py
+++ 2021/day02/solution.py
@@ -1,2 +1,16 @@
 def part1(data):
-    return "day2"
+    input = data.splitlines()
+    depth = 0
+    hpos = 0
+    
+    for direction in input:
+        units = int(direction[(len(direction)-1)])
+
+        if "forward" in direction:
+            hpos += units
+        elif "down" in direction:
+            depth += units
+        elif "up" in direction:
+            depth -= units
+
+    return hpos * depth
