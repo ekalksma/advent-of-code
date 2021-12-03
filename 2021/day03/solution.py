97737510b49c6859fddfcb89a674712fdecbd71c
ekalksma <emiel.kalksma@gmail.com>
2021-12-03 19:08:46 +0100

    Add day3 solution part1

--- 2021/day03/solution.py
+++ 2021/day03/solution.py
@@ -1,2 +1,40 @@
 def part1(data):
-    return "day03"
+    input = data.splitlines()
+    input_length = len(input)
+    bin_length = len(input[0])
+    gamma = ""
+    epsilon = ""
+    ones = 0
+    zeros = 0
+
+    for i in range(0,bin_length):
+        for bin in input:
+            if str(bin[i]) == "1":
+                ones += 1 
+
+        zeros = input_length - ones
+
+        if ones > zeros:
+            gamma += "1"
+        else:
+            gamma += "0"
+        ones = 0
+    
+    epsilon = inverse_bin(gamma)
+    gamma = int(gamma, 2)
+    epsilon = int(epsilon, 2)
+
+    return gamma * epsilon
+
+def inverse_bin(bin):
+    inverse_s = ""
+
+    for bit in bin:
+
+        if bit == "0":
+            inverse_s += "1"
+
+        elif bit == "1":
+            inverse_s += "0"
+
+    return inverse_s
