40ee17ec27ca6b2e740ea2a1ff7b67c29219c015
ekalksma <emiel.kalksma@gmail.com>
2022-12-03 08:14:19 -0300

    Add day03 solution

---
+++ 2022/day03/__init__.py
@@ -0,0 +1,1 @@
+from day03.solution import *
---
+++ 2022/day03/solution.py
@@ -0,0 +1,33 @@
+def part1(data):
+    priority = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+    'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+    , 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39,
+    'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52 }
+    input = data.splitlines()
+    total_points = 0
+
+    for line in input:
+        first_half = set(line[:int(len(line) / 2)])
+        second_half = set(line[int(len(line) / 2):])
+        common = first_half.intersection(second_half)
+        total_points += priority[list(common)[0]]
+    
+    return total_points
+def part2(data):
+    input = data.splitlines()
+    priority = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10, 'k': 11, 'l': 12, 'm': 13,
+    'n': 14, 'o': 15, 'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20, 'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25, 'z': 26
+    , 'A': 27, 'B': 28, 'C': 29, 'D': 30, 'E': 31, 'F': 32, 'G': 33, 'H': 34, 'I': 35, 'J': 36, 'K': 37, 'L': 38, 'M': 39,
+    'N': 40, 'O': 41, 'P': 42, 'Q': 43, 'R': 44, 'S': 45, 'T': 46, 'U': 47, 'V': 48, 'W': 49, 'X': 50, 'Y': 51, 'Z': 52 }
+    total_points = 0
+    
+    for x in range(3, len(input) + 1, 3):
+        first = set(input[x-3])
+        second = set(input[x-2])
+        third = set(input[x-1])
+
+        common = set.intersection(first, second, third)
+
+        total_points += priority[list(common)[0]]
+    
+    return total_points
