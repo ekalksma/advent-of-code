48233af40c81626a017d075f6c5c8a3da6d4b520
ekalksma <emiel.kalksma@gmail.com>
2022-12-04 09:23:14 -0300

    Refactor

--- 2022/day04/solution.py
+++ 2022/day04/solution.py
@@ -18,11 +18,8 @@ def part2(data):
         pairs = line.split(",")
         pair_one = pairs[0].split("-")
         pair_two = pairs[1].split("-")
-        
-        if ((int(pair_one[0]) >= int(pair_two[0]) and int(pair_one[0]) <= int(pair_two[1])) or
-         (int(pair_one[1]) >= int(pair_two[0]) and int(pair_one[0]) <= int(pair_two[1])) or 
-         (int(pair_two[0]) >= int(pair_one[0]) and int(pair_two[0]) <= int(pair_one[1])) or 
-         (int(pair_two[1]) >= int(pair_one[0]) and int(pair_two[0]) <= int(pair_one[1]))):
+
+        if int(pair_two[0]) <= int(pair_one[1]) and int(pair_two[1]) >= int(pair_one[0]):
             num_pairs += 1
 
     return num_pairs
